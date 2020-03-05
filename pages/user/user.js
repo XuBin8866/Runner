0bd6175444c4629236210ece774e676714a21e25
AlwaysXu <1205817366@qq.com>
2020-03-05 14:33:03 +0800

    添加个人用户界面（不完善）

---
+++ pages/user/user.js
@@ -0,0 +1,71 @@
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    myPicture: '/images/myPicture.png',
+    markPicture: '/images/mark.png',
+    stuName: '张运楠',
+    stuId: '20182427',
+    step: '13333',
+    moInter: '23',
+    acInter: '56',
+    rnkInter: '38'
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
